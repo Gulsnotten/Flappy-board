2e2a031ef56b57c4484d10509dda95aedafc9352
Gulsnotten <eriksall1@gmail.com>
2017-01-21 18:25:47 +0100

    Restart added

---
+++ Flappy board/Assets/script/PlayerIDScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIDScript : MonoBehaviour {
+    public int m_inputIndex;
+    public int m_animationIndex;
+    public InputModuleScript m_input;
+    public AnimatorOverrideController m_animation;
+}
---
+++ Flappy board/Assets/script/PlayerSlotScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlotScript : MonoBehaviour {
+    bool m_pickedSkin;
+    
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

0854129fe74e23b053a9776e4b88b736bc7e2d87
Gulsnotten <eriksall1@gmail.com>
2017-01-22 06:45:47 +0100

    FUCK

--- Flappy board/Assets/script/TootScript.cs
+++ Flappy board/Assets/script/TootScript.cs
@@ -7,20 +7,20 @@ public class TootScript : MonoBehaviour {
     public float m_minPitch = -0.2f;
     public float m_maxPitch = 0.2f;
     public AudioClip[] m_angry;
-    public float m_volume = 1f;
 
     AudioSource m_source;
+    const float tootVol = 0.3f;
 
     // Use this for initialization
     void Start () {
         m_source = GetComponent<AudioSource>();
-        m_source.pitch = 1 + Random.Range(m_minPitch, m_maxPitch);
         tooted = false;
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        m_source.PlayOneShot(m_angry[Random.Range(0, m_angry.Length)], 1f);
+        m_source.pitch = 1f;
+        m_source.PlayOneShot(m_angry[Random.Range(0, m_angry.Length)], 0.7f);
         
         Animator ani = GetComponentInChildren<Animator>();
         ani.ResetTrigger("rustle");
@@ -28,7 +28,8 @@ public class TootScript : MonoBehaviour {
 
         if (!tooted) {
             tooted = true;
-            m_source.Play();
+            m_source.pitch = 1 + Random.Range(m_minPitch, m_maxPitch);
+            m_source.PlayOneShot(m_source.clip, tootVol);
         }
     }
 }
